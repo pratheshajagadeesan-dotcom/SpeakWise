01061a13e0081bfa4c201bfb6d20ee6a2357a0d0
prathesha26 <pratheshajagadeesan@gmail.com>
2026-07-01 09:47:50 +0530

    Day 1: Spring Boot project setup and MySQL configuration

---
+++ src/main/java/com/speakwise/SpeakWiseApplication.java
@@ -0,0 +1,13 @@
+package com.speakwise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeakWiseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpeakWiseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/speakwise/SpeakWiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.speakwise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeakWiseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
